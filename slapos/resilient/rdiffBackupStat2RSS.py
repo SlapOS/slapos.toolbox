d3829d9f30430b7f2a7d75e76a3bd856b85a634e
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2016-09-08 16:33:59 +0200

    resilient: adds test for rdiffBackupStat2RSS

--- slapos/resilient/rdiffBackupStat2RSS.py
+++ slapos/resilient/rdiffBackupStat2RSS.py
@@ -33,40 +33,43 @@ def makeDictFromStatFile(text_content):
             'value': m.group(2),
             'human_readable_value': m.group(2)}
 
-def getRSSItemFromDict(item, parser):
+def getRSSItemFromDict(item, option):
   description = "Metric;Value;Human Readable Value\n"
   for entry in item:
     description += "%s;%s;%s\n" % (entry['metric'], entry['value'], entry['human_readable_value'])
-    if entry['metric'] == "StopTime":
-      pubDate = datetime.datetime.fromtimestamp(float(item['value']))
+    if entry['metric'] == "EndTime":
+      pubDate = datetime.datetime.fromtimestamp(float(entry['value']))
   return RSS2.RSSItem(
     title="Rdiff-Backup Transfer Statistics",
-    link=parser.feed_url,
+    link=option.feed_url,
+    pubDate=pubDate,
     description=description)
 
-def genrss():
+def genRSS(option):
   """
   Read statistics file from rdiff-backup and generate a RSS feed entry from it
   """
-  parser = parseArguments()
-
-  stat_file_list = sorted([file for file in os.listdir(parser.rdiff_backup_data_folder)
+  stat_file_list = sorted([file for file in os.listdir(option.rdiff_backup_data_folder)
                            if file.startswith('session_statistics')])
 
   item_dict = OrderedDict()
   for stat_file in stat_file_list:
-    with open(os.path.join(parser.rdiff_backup_data_folder, stat_file), 'r') as file:
+    with open(os.path.join(option.rdiff_backup_data_folder, stat_file), 'r') as file:
       item_dict[stat_file] = [makeDictFromStatFile(line.strip()) for line in file.readlines()]
-
   title="Rdiff-Backup Statistics"
   rss_feed = RSS2.RSS2(
     title=title,
-    link=parser.feed_url,
+    link=option.feed_url,
     description=title,
-    items = [getRSSItemFromDict(item_dict[item], parser) for item in item_dict])
+    items = [getRSSItemFromDict(item_dict[item], option) for item in item_dict])
 
-  with open(parser.output, 'w') as rss_file:
-    rss_file.write(rss_feed.to_xml())
+  return rss_feed.to_xml()
 
 def main():
-  exit(genrss())
+  option = parseArguments()
+  feed = genRSS(option)
+
+  with open(option.output, 'w') as rss_file:
+    rss_file.write(feed)
+
+  exit(0)
