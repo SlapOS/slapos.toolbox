e91675eed0207450e5d307b243c8eae3efa1d19a
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2016-09-08 12:41:14 +0200

    resilient: adds test for checkFeedAsPromise

--- slapos/resilient/checkFeedAsPromise.py
+++ slapos/resilient/checkFeedAsPromise.py
@@ -1,12 +1,14 @@
 # Command line script to test a RSS feed in a promise
 # Checks that a given pattern can be found (or not) in the title or the
 # description of the latest feed item.
-# A time buffer option can be given, so the promise really gets true, or false,
-# only if the last item is older or younger than the allowed time buffer.
+# A time buffer option can be given, to determine if the emitter process is in
+# a stalled state, in the case that no OK pattern has been found
+
 import argparse
 import datetime
 import feedparser
 import sys
+
 
 def parseArguments():
   parser = argparse.ArgumentParser()
@@ -23,8 +25,10 @@ def parseArguments():
                       default=[],
                       help='If this pattern is found, then promise fails')
   parser.add_argument('--time-buffer', dest='time_buffer', type=int,
+                      default=0,
                       help='Time delta in seconds before the promise really succeeds or fails')
   return parser.parse_args()
+
 
 def containsPattern(string, pattern_list):
   for pattern in pattern_list:
@@ -32,12 +36,12 @@ def containsPattern(string, pattern_list):
       return True
   return False
 
-def main():
-  option = parseArguments()
-  feed = feedparser.parse(option.feed_path)
+
+def checkFeedAsPromise(feed, option):
+  feed = feedparser.parse(feed)
 
   if feed.bozo:
-    sys.exit('Feed malformed')
+    return 'Feed malformed'
 
   last_item = feed.entries[-1]
   if option.title:
@@ -45,7 +49,7 @@ def main():
   elif option.description:
     candidate_string = last_item.description
   else:
-    sys.exit('At least one in [--title|--description] should be provided')
+    return 'At least one in [--title|--description] should be provided'
 
   publication_date = datetime.datetime(*last_item.published_parsed[:7])
   publication_age = datetime.datetime.now() - publication_date
@@ -55,25 +59,36 @@ def main():
   ko_pattern_found = containsPattern(candidate_string, option.ko_pattern_list)
 
   if ok_pattern_found and ko_pattern_found:
-    sys.exit('Both OK and KO patterns found: please check arguments')
+    return 'Both OK and KO patterns found: please check arguments'
 
   # Expectations fulfilled
   if ok_pattern_found:
-      sys.exit(0)
+      return ''
 
   if ko_pattern_found:
-    sys.exit('KO pattern found')
+    return 'KO pattern found'
 
   if not ok_pattern_found:
     if publication_age < time_buffer:
       # We have to wait for buffer to expire
-      sys.exit(0)
+      return ''
     else:
       # If time-buffer is out, we are in stalled state
-      sys.exit('Stalled situation')
+      return 'Stalled situation'
 
   # If not ok, and not stalled, what can have possibly happen ?
-  sys.exit(1)
+  return 'Something went wrong'
+
+
+def main():
+  option = parseArguments()
+  result = checkFeedAsPromise(option.feed_path, option)
+
+  if len(result) > 0:
+    sys.exit(result)
+  else:
+    sys.exit(0)
+
 
 if __name__ == '__main__':
   main()
