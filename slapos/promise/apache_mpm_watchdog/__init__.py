3206d6d1a12f42e9492ba4d2645827e20d74ec91
Rafael Monnerat <rafael@nexedi.com>
2017-05-30 19:01:49 +0200

    apache_mpm_watchdog: Clean up and add tests.

--- slapos/promise/apache_mpm_watchdog/__init__.py
+++ slapos/promise/apache_mpm_watchdog/__init__.py
@@ -8,18 +8,41 @@ import time
 
 search_pid_regex = r"</td><td.*?>(.+?)</td><td>yes \(old gen\)</td>"
 
-def main(url, user, password, db_path, timeout=600):
+def loadJSONFile(db_path):
   if os.path.exists(db_path):
     with open(db_path) as json_file:
        try:
-         pid_dict = json.load(json_file)
+         return json.load(json_file)
        except ValueError:
-         pid_dict = {}
+         return {}
   else:
-    pid_dict = {}
+    return {}
 
-  r = requests.get(url, auth=(user, password))
-  for i in re.findall(search_pid_regex, r.text):
+def writeJSONFile(pid_dict, db_path):
+  if db_path is None:
+    # No place to save
+    return 
+  for pid in pid_dict.copy():
+    try:
+      process = psutil.Process(int(pid))
+    except psutil.NoSuchProcess:
+      del pid_dict[pid]
+
+  with open(db_path, "w") as f:
+    f.write(json.dumps(pid_dict))
+
+def getServerStatus(url, user, password):
+  if user is not None:
+    r = requests.get(url, auth=(user, password))
+  else:
+    r = requests.get(url)
+
+  if r.status_code == 200:
+    return r.text
+
+def watchServerStatus(pid_dict, server_status):
+  _pid_dict = pid_dict.copy()
+  for i in re.findall(search_pid_regex, server_status):
     try:
       process = psutil.Process(int(i))
     except psutil.NoSuchProcess:
@@ -32,12 +55,28 @@ def main(url, user, password, db_path, timeout=600):
         print "Sending signal -%s to %s" % (signal.SIGKILL, i)
         os.kill(int(i), signal.SIGKILL)
 
-  for i in pid_dict.copy():
-    try:
-      process = psutil.Process(int(i))
-    except psutil.NoSuchProcess:
-      del pid_dict[i]
+  return _pid_dict
 
-  with open(db_path, "w") as f:
-    f.write(json.dumps(pid_dict))
+def main():
+  parser = argparse.ArgumentParser()
+  # Address to ping to
+  parser.add_argument("-u", "--url", required=True)
+  # Force use ipv4 protocol
+  parser.add_argument("-u", "--user")
+  parser.add_argument("-p", "--password")
+  parser.add_argument("-d", "--db")
+  parser.add_argument("-t", "--timeout", default=600)
+  args = parser.parse_args()
+
+  pid_dict = loadJSONFile(args.db)
+
+  server_status = getServerStatus(
+    args.url, args.user, args.password)
+
+  if server_status is None:
+    raise ValueError("Couldn't connect to server status page")
+
+  pid_dict = watchServerStatus(pid_dict, server_status)
+
+  writeJSONFile(pid_dict, args.db)
 
