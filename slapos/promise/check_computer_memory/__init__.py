bd09f20e4448496f8e7e15afa62c1bdda56b4415
Tristan Cavelier <tristan.cavelier@nexedi.com>
2018-02-16 14:08:17 +0000

    check_computer_memory: be more consistent with getFreeMemory return value
    avoid to return exit code (int) or mem (object)
    better return a tuple (mem, error_message) (object, str)

--- slapos/promise/check_computer_memory/__init__.py
+++ slapos/promise/check_computer_memory/__init__.py
@@ -23,8 +23,7 @@ def getFreeMemory(database, time, date):
     query_result = database.select("computer", date, "memory_size", limit=1) 
     result = zip(*query_result)
     if not result or not result[0][0]:
-      print "couldn't fetch total memory, collectordb is empty?"
-      return 0
+      return (None, "couldn't fetch total memory, collectordb is empty?")
     mem['total'] = result[0][0]
 
     # fetch free and used memory 
@@ -32,17 +31,15 @@ def getFreeMemory(database, time, date):
     query_result = database.select("system", date, "memory_free, memory_used", where=where_query)
     result = zip(*query_result)
     if not result or not result[0][0]: 
-      print "couldn't fetch free memory"
-      return 0
+      return (None, "couldn't fetch free memory")
     mem['free'] = result[0][0]
     if not result or not result[1][0]: 
-      print "couldn't fetch used memory"
-      return 0
+      return (None, "couldn't fetch used memory")
     mem['used'] = result[1][0]
   finally:
     database.close()
 
-  return mem
+  return (mem, "")
 
 def main():
   parser = argparse.ArgumentParser()
@@ -58,11 +55,12 @@ def main():
   db_path = args.collectordb
 
   if db_path.endswith("collector.db"):db_path=db_path[:-len("collector.db")]
-  memory = getFreeMemory(db_path, currenttime, currentdate)
+  memory, error = getFreeMemory(db_path, currenttime, currentdate)
+  if error:
+    print error
+    return 0
   threshold = float(memory['total']) * 0.2
 
-  if memory is 0:
-    return 0
   if memory['used'] <= threshold:
     print "OK - memory used: {:d}% ({}B of {}B)".format(memory["used"] * 100 / memory["total"], memory["used"], memory["total"])
     return 0
