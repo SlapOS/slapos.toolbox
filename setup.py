ebc77e1fc1787e4cd80b069cacbd945ab8b8195b
Alain Takoudjou <alain.takoudjou@nexedi.com>
2018-07-31 11:02:45 +0200

    Release 0.77

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.76'
+version = '0.77'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
