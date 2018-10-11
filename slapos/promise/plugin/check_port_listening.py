18cc8e9f432cde3604e6f8d3f1429b5bb5eea8d7
Thomas Gambier <thomas.gambier@nexedi.com>
2018-10-11 14:43:48 +0200

    promise: check_port_listening is now available with new promise mechanism
    This promise will replace the old check_port_listening promise (https://lab.nexedi.com/nexedi/slapos/blob/master/slapos/recipe/check_port_listening.py). See https://www.erp5.com/slapos-TechnicalNote.General.SlapOS.Monitoring.Specifications for how to instantiate this promise in buildout.

---
+++ slapos/promise/plugin/check_port_listening.py
@@ -0,0 +1,45 @@
+from zope import interface as zope_interface
+from slapos.grid.promise import interface
+from slapos.grid.promise.generic import GenericPromise
+
+import socket
+import sys
+
+class RunPromise(GenericPromise):
+
+  zope_interface.implements(interface.IPromise)
+
+  def __init__(self, config):
+    GenericPromise.__init__(self, config)
+    # check port is listening at least every 2 minutes
+    self.setPeriodicity(minute=2)
+
+  def sense(self):
+    """
+      Simply test if we can connect to specified host:port.
+    """
+    hostname = self.getConfig('hostname')
+    port = self.getConfig('port')
+
+    addr = (hostname , port)
+    try:
+      socket.create_connection(addr).close()
+    except (socket.herror, socket.gaierror), e:
+      self.logger.error("hostname/port ({}) is not correct: {}".format(addr, e))
+    except (socket.error, socket.timeout), e:
+      self.logger.error("ERROR while connecting to {}: {}".format(addr, e))
+    else:
+      self.logger.info("port connection OK ({})".format(addr))
+
+  # no need to define Test as it is the default implementation
+  #def test(self):
+  #  """
+  #    Test is failing if last sense was bad.
+  #  """
+  #  return self._test(result_count=1, failure_amount=1)
+
+  def anomaly(self):
+    """
+      There is an anomaly if last 3 senses were bad.
+    """
+    return self._test(result_count=3, failure_amount=3)
