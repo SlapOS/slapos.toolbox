0dc7caf568570e25b1cd34ecff313bc193a788be
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2018-11-28 06:29:26 +0100

    resilient: exclude files using fnmatch instead of regexp
    As mentionned by @jerome in 2848a965b0380b1fd3d7942ec572b99c0cfec7d8
    fnmatch allows more natural definitions for path manipulations

--- slapos/resilient/identity_script_excluding_path.py
+++ slapos/resilient/identity_script_excluding_path.py
@@ -2,9 +2,9 @@ from __future__ import print_function
 
 import argparse
 import os
-import subprocess
 import re
 
+from fnmatch import fnmatch
 from .runner_utils import *
 
 def parseArgumentList():
@@ -25,11 +25,10 @@ def calculateSignature():
   result = re.match('^\.\/runner\/instance\/slappart(\d)', file_list[0])
   base_relative_path = result.group()
   excluded_path_list = ['./' + os.path.relpath(os.path.join(base_relative_path, path)) for path in args.exclude_path]
-  excluded_path_regex_list = [re.compile(excluded_path) for excluded_path in excluded_path_list]
 
   filtered_file_list = [
     x for x in file_list
-    if not any([re.match(excluded_path_regex, x) for excluded_path_regex in excluded_path_regex_list])
+    if not any([fnmatch(x, excluded_path) for excluded_path in excluded_path_list])
   ]
 
   print('\n'.join(getSha256Sum(filtered_file_list)))
