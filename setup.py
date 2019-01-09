25f89c210903339c203635164817dbf105940178
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2019-01-09 14:29:40 +0100

    release 0.85

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.84'
+version = '0.85'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
