d38c7287c04bb5b7060b4b422583afc890989570
Bryton Lacquement <bryton.lacquement@nexedi.com>
2019-06-20 11:09:15 +0200

    wip

--- slapos/test/promise/plugin/test_check_url_available.py
+++ slapos/test/promise/plugin/test_check_url_available.py
@@ -197,6 +197,8 @@ class CheckUrlAvailableMixin(TestPromisePluginMixin):
   def tearDownClass(cls):
     cls.server_process.terminate()
     cls.server_process.join()
+    # In Py3, the server socket does not seem to be automatically closed.
+    cls.server_process._target.__self__.socket.close()
     for p in [
       cls.test_server_certificate_file.name,
       cls.test_server_ca_certificate_file.name,
