05582c0a769903ceedf5fd7f6e39f8a095dcecfc
Łukasz Nowak <luke@nexedi.com>
2019-12-02 16:59:45 +0100

    Release 0.98

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.97'
+version = '0.98'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
