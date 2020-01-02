c3eeed9dad0e67b9aa719e6c58b7ba9a19c04bc8
Łukasz Nowak <luke@nexedi.com>
2020-01-02 10:10:48 +0100

    Release 0.103
    Fixed check_surykatka_json for http-only sites

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.102'
+version = '0.103'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
