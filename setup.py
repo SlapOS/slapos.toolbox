f09df509c0ce253e78845955bb3f55c02150746d
Łukasz Nowak <luke@nexedi.com>
2020-02-12 15:06:53 +0100

    Release 0.105

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.104'
+version = '0.105'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
