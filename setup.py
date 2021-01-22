68e5bd44c6d6c07605893524d49a6bbcde304f3f
Jérome Perrin <jerome@nexedi.com>
2021-01-22 14:23:47 +0900

    Release 0.114

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.113'
+version = '0.114'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
