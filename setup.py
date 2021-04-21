ea09102fa990538055f008f06e1349a860ac90e5
Jérome Perrin <perrinjerome@gmail.com>
2021-04-21 10:06:22 +0900

    Release 0.119

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.118'
+version = '0.119'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
