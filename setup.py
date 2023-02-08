c68f8faf6a117c87e0a376c5f049d86e46080d19
Joanne Hugé <joanne.huge@nexedi.com>
2023-02-08 17:41:47 +0100

    Release 0.130

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.129'
+version = '0.130'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
