91e7b55962efb61db125191dc3c9757f7e47548b
Łukasz Nowak <luke@nexedi.com>
2023-03-20 08:43:18 +0100

    Release 0.134

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.133'
+version = '0.134'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
