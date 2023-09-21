268a82aec228f4a95f9b88d3850c576c1f2cb123
lu.xu <lu.xu@nexedi.com>
2023-09-21 16:37:08 +0200

    Release 0.138

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.137'
+version = '0.138'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
