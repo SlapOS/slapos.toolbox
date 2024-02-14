6419103b7f2693ffd21932ac78c5d632ef8997d8
Kirill Smelkov <kirr@nexedi.com>
2024-02-14 12:41:58 +0300

    Release 0.141

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.140'
+version = '0.141'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
