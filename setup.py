09c87a1d309f376fcf51cec252d767e6330f8e77
Jérome Perrin <jerome@nexedi.com>
2025-10-21 19:00:18 +0900

    Release 0.128.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.128.3'
+version = '0.128.4'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
