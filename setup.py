6290d9eeac94d21c3c7228bcc94d93a0020bfcdb
Thomas Gambier <thomas.gambier@nexedi.com>
2025-11-10 09:17:16 +0100

    Release 0.160

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.159'
+version = '0.160'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
