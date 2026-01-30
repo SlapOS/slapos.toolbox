72fd8c02a5ba3e1697b50936186226ae40b670ff
Joanne Hugé <joanne.huge@nexedi.com>
2026-01-30 11:19:01 +0000

    promise/plugin: update Amarisoft stats log name
    To match software release

--- slapos/test/promise/plugin/test_check_rx_saturated.py
+++ slapos/test/promise/plugin/test_check_rx_saturated.py
@@ -41,9 +41,9 @@ class TestCheckRXSaturated(TestPromisePluginMixin):
     super(TestCheckRXSaturated, self).setUp()
     self.amarisoft_stats_log = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'amarisoft_stats.json.log')
     with open(self.amarisoft_stats_log, 'w+') as f:
-      f.write("""{"time": "%s", "log_level": "INFO", "message": "Samples stats", "data": {"samples": {"rx": [{"max": %f}, {"max": %f}]}}}
-{"time": "%s", "log_level": "INFO", "message": "Samples stats", "data": {"samples": {"rx": [{"max": %f}, {"max": %f}]}}}
-{"time": "%s", "log_level": "INFO", "message": "Samples stats", "data": {"samples": {"rx": [{"max": %f}, {"max": %f}]}}}""" % (
+      f.write("""{"time": "%s", "log_level": "INFO", "message": "Amarisoft Stats", "data": {"samples": {"rx": [{"max": %f}, {"max": %f}]}}}
+{"time": "%s", "log_level": "INFO", "message": "Amarisoft Stats", "data": {"samples": {"rx": [{"max": %f}, {"max": %f}]}}}
+{"time": "%s", "log_level": "INFO", "message": "Amarisoft Stats", "data": {"samples": {"rx": [{"max": %f}, {"max": %f}]}}}""" % (
       (datetime.now() - timedelta(seconds=25)).strftime("%Y-%m-%d %H:%M:%S,%f")[:-3], +99, -6.0,
       (datetime.now() - timedelta(seconds=15)).strftime("%Y-%m-%d %H:%M:%S,%f")[:-3], +99, -3.0,
       (datetime.now() - timedelta(seconds=5)).strftime("%Y-%m-%d %H:%M:%S,%f")[:-3],  +99, -7.0,
