efe308888a8f23136da7679d1dfffa86e9b1e9b4
Julien Muchembled <jm@nexedi.com>
2026-05-21 15:37:33 +0200

    Release 0.167

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.166'
+version = '0.167'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
